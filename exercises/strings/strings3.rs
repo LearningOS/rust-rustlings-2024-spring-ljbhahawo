9479cfb7b89faadb3089552c7ed0dfd500b8ce96
root <root@ljb.localdomain>
2024-04-08 15:24:05 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,23 +3,27 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut res = String::from(input);
+    res+=" world!";
+    res
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if input.contains("cars"){
+        input.replace("cars","balloons")
+    }else{
+        input.to_string()
+    }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
