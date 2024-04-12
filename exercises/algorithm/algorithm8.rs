9c741fd215992e53ae4c778e79af2bb2a2db2866
root <root@ljb.localdomain>
2024-04-12 22:53:33 +0800

    up algo8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,7 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -52,30 +52,56 @@ impl<T> Default for Queue<T> {
     }
 }
 
-pub struct myStack<T>
+pub struct MyStack<T>
 {
-	//TODO
+	// true为q1，false为q2
+    
+    main_queue:bool,
 	q1:Queue<T>,
 	q2:Queue<T>
 }
-impl<T> myStack<T> {
+impl<T> MyStack<T> {
     pub fn new() -> Self {
         Self {
-			//TODO
+            // 默认主队列为q1            
+            main_queue:true,
 			q1:Queue::<T>::new(),
 			q2:Queue::<T>::new()
+            
         }
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        if self.main_queue {
+            self.q1.enqueue(elem);
+        }else {
+            self.q2.enqueue(elem)
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        // pop的时候，将目前的主队列的最后一个元素前的所有元素，放入另一个队列
+        if self.q1.is_empty() && self.q2.is_empty(){
+            Err("Stack is empty")
+        }else {
+            if self.main_queue{
+                while self.q1.size() >1{
+                    self.q2.enqueue(self.q1.dequeue().unwrap());
+                }
+                self.main_queue = false;
+                self.q1.dequeue()
+            }else {
+                while self.q2.size() >1{
+                    self.q1.enqueue(self.q2.dequeue().unwrap());
+                }
+                self.main_queue = true;
+                self.q2.dequeue()
+            }
+        }
+		
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+		self.q1.is_empty() && self.q2.is_empty()
+        
     }
 }
 
@@ -85,7 +111,7 @@ mod tests {
 	
 	#[test]
 	fn test_queue(){
-		let mut s = myStack::<i32>::new();
+		let mut s = MyStack::<i32>::new();
 		assert_eq!(s.pop(), Err("Stack is empty"));
         s.push(1);
         s.push(2);
--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -37,7 +37,15 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.count += 1;
+        self.items.push(value);
+
+        let mut idx = self.count;
+
+        while idx > 1 && (self.comparator)(&self.items[idx], &self.items[self.parent_idx(idx)]) {
+            self.items.swap(idx, self.parent_idx(idx));
+            idx = self.parent_idx(idx);
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +65,11 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        if self.right_child_idx(idx) > self.count || (self.comparator)(&self.items[self.left_child_idx(idx)], &self.items[self.right_child_idx(idx)]) {
+            self.left_child_idx(idx)
+        } else {
+            self.right_child_idx(idx)
+        }
     }
 }
 
@@ -84,8 +95,25 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.is_empty() {
+            None
+        } else {
+            let min = self.items.swap_remove(1);
+            self.count -= 1;
+
+            let mut idx = 1;
+            while self.children_present(idx) {
+                let smaller_child_idx = self.smallest_child_idx(idx);
+
+                if (self.comparator)(&self.items[smaller_child_idx], &self.items[idx]) {
+                    self.items.swap(smaller_child_idx, idx);
+                    idx = smaller_child_idx;
+                } else {
+                    break;
+                }
+            }
+            Some(min)
+        }
     }
 }
 
